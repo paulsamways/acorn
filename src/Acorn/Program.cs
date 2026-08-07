ee0d745b87e5adf228968b354a6aaab985878adb
Paul Samways <paul@samways.id.au>
2026-08-08 05:54:34 +1000

    add pipeline

--- src/Acorn/Program.cs
+++ src/Acorn/Program.cs
@@ -104,6 +104,12 @@ internal class Program
 
     var app = builder.Build();
 
+    using (var scope = app.Services.CreateScope())
+    {
+      var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+      dbContext.Database.Migrate();
+    }
+
     // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
     {
